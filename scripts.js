c2ea9694bfed0272ee0d9f734ec49de46655f446
Nate Lee <nleepercussion@gmail.com>
2021-05-27 20:19:19 -0400

    Updated app with text input

--- scripts.js
+++ scripts.js
@@ -1,27 +1,37 @@
 'use strict';
 
-const tempElement = document.getElementById('temp');
-const descriptionElement = document.getElementById('description');
-const humidityElement = document.getElementById('humidity');
-const getWeatherInfo = document.getElementById('getWeatherInfo');
+const apiKey = 'de4fcf6028dfe9ccf2513aa919b22358'
+const generateWeather = document.getElementById('generateWeather');
 
 
-    //function to convert Kelvin to farenheit
-function kToF(k) {
-    return ((k-273.15) * 9/5 + 32).toFixed(1)
-}
-
-getWeatherInfo.addEventListener('click', function() {
-    fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=de4fcf6028dfe9ccf2513aa919b22358"
-    )
-    .then(response => response.json())
+generateWeather.addEventListener('click', function() {
+    const inputElement = document.querySelector('input');
+    const placeholderElement = document.querySelector('span');
+    
+    placeholderElement.innerHTML = inputElement.value
+    getWeatherInfo(inputElement.value);
+});   
+function getWeatherInfo (city) {
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=de4fcf6028dfe9ccf2513aa919b22358`
+    
+    fetch(url).then(response => response.json())
     .then(data => {
-        console.log(data);
-        console.log(data.weather[0].description);
-        descriptionElement.innerHTML = data.weather[0].description;
-        tempElement.innerHTML = kToF(data.main.temp) + "°F";
-        humidityElement.innerHTML = data.main.humidity
+        console.log(data.main.humidity)
+        updateWeather(data.main.feels_like, data.weather[0].description, data.main.temp_max, data.main.temp_min, data.main.humidity)
     })
     .catch(error => console.log("error"))
-})
+};
+
+function updateWeather(currentTemp, currentConditions, todayHigh, todayLow, currentHumidity) {
+    const div1 = document.querySelector('#currentTemp');
+    div1.innerHTML = (currentTemp).toFixed(1);
+    const div2 = document.querySelector('#currentConditions')
+    div2.innerHTML = currentConditions;
+    const div3 = document.querySelector('#todayHigh');
+    div3.innerHTML = todayHigh.toFixed(1);
+    const div4 = document.querySelector('#todayLow');
+    div4.innerHTML = todayLow.toFixed(1);
+    const div5 = document.querySelector('#currentHumidity');
+    div5.innerHTML = currentHumidity;
+}
+
